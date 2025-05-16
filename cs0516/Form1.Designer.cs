144681d151b4c4079960bbc5801a3e35abae9f2f
YuTanaka <tanakaedu@users.noreply.github.com>
2025-05-16 15:55:08 +0900

    変数

--- cs0516/Form1.Designer.cs
+++ cs0516/Form1.Designer.cs
@@ -28,12 +28,111 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            components = new System.ComponentModel.Container();
+            button1 = new Button();
+            label1 = new Label();
+            button2 = new Button();
+            button3 = new Button();
+            textBox1 = new TextBox();
+            timer1 = new System.Windows.Forms.Timer(components);
+            button4 = new Button();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Location = new Point(21, 24);
+            button1.Margin = new Padding(5, 6, 5, 6);
+            button1.Name = "button1";
+            button1.Size = new Size(213, 73);
+            button1.TabIndex = 0;
+            button1.Text = "button1";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(279, 323);
+            label1.Margin = new Padding(5, 0, 5, 0);
+            label1.Name = "label1";
+            label1.Size = new Size(71, 30);
+            label1.TabIndex = 1;
+            label1.Text = "label1";
+            // 
+            // button2
+            // 
+            button2.Location = new Point(21, 109);
+            button2.Margin = new Padding(5, 6, 5, 6);
+            button2.Name = "button2";
+            button2.Size = new Size(213, 81);
+            button2.TabIndex = 2;
+            button2.Text = "button2";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(21, 202);
+            button3.Margin = new Padding(5, 6, 5, 6);
+            button3.Name = "button3";
+            button3.Size = new Size(213, 73);
+            button3.TabIndex = 3;
+            button3.Text = "button3";
+            button3.UseVisualStyleBackColor = true;
+            button3.Click += button3_Click;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(279, 24);
+            textBox1.Margin = new Padding(5, 6, 5, 6);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(330, 36);
+            textBox1.TabIndex = 4;
+            textBox1.Text = "TANAKA Yu";
+            // 
+            // timer1
+            // 
+            timer1.Enabled = true;
+            timer1.Interval = 20;
+            timer1.Tick += timer1_Tick;
+            // 
+            // button4
+            // 
+            button4.Location = new Point(21, 284);
+            button4.Name = "button4";
+            button4.Size = new Size(213, 69);
+            button4.TabIndex = 5;
+            button4.Text = "button4";
+            button4.UseVisualStyleBackColor = true;
+            button4.Click += button4_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(12F, 30F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(696, 396);
+            Controls.Add(button4);
+            Controls.Add(textBox1);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(label1);
+            Controls.Add(button1);
+            Font = new Font("Yu Gothic UI", 16F);
+            Margin = new Padding(5, 6, 5, 6);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Button button1;
+        private Label label1;
+        private Button button2;
+        private Button button3;
+        private TextBox textBox1;
+        private System.Windows.Forms.Timer timer1;
+        private Button button4;
     }
 }
--- cs0516/Form1.cs
+++ cs0516/Form1.cs
@@ -6,5 +6,64 @@ namespace cs0516
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("‚±‚ñ‚É‚¿‚Í");
+            MessageBox.Show(100.ToString());
+            MessageBox.Show($"{100}");
+            MessageBox.Show($"label1.Top={label1.Top}");
+            MessageBox.Show(3.14f.ToString());
+            MessageBox.Show($"{3.14f}");
+
+            MessageBox.Show(1 + 1.ToString());//11
+            MessageBox.Show((1 + 1).ToString());//2
+            MessageBox.Show($"{1 + 1}");//2
+
+            MessageBox.Show((1.23f * 2.34f).ToString());
+            MessageBox.Show($"{1.23f * 2.34f}");
+
+            MessageBox.Show("1" + "1");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int a = 1;
+            a += 5;
+            MessageBox.Show($"{a}");
+            a -= 5;
+            MessageBox.Show($"{a}");
+            a *= 5;
+            MessageBox.Show($"{a}");
+            a /= 5;
+            MessageBox.Show($"{a}");
+            a %= 5;
+            MessageBox.Show($"{a}");
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Hello");
+            label1.Text = "Hello";
+            MessageBox.Show($"‚±‚ñ‚É‚¿‚Í{textBox1.Text}‚³‚ñ");
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            label1.Left--;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            int a;
+            float fw;
+            string str;
+            bool bl;
+
+            int b = 10;
+            float fw2 = -10.5f;
+            string str2 = "‚±‚ñ‚É‚¿‚Í";
+            bool bl2 = true;
+        }
     }
 }
